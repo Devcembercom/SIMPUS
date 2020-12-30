9e0065877c44990800ac3b2467cf5a40b0330b8c
revanapriyandi <revanapriyandi19@gmail.com>
2020-12-30 22:10:01 +0700

    view identitas k1k4

--- app/Http/Controllers/KesgaGizi/IdentitasK1K4Controller.php
+++ app/Http/Controllers/KesgaGizi/IdentitasK1K4Controller.php
@@ -9,6 +9,6 @@ class IdentitasK1K4Controller extends Controller
 {
     public function index()
     {
-        return abort('503', 'Proses Penyiapan');
+        return view('KesgaGizi.IdentitasK1K4.index');
     }
 }
---
+++ resources/views/KesgaGizi/IdentitasK1K4/table/tableK4.blade.php
@@ -0,0 +1,23 @@
+<x-card class="card-primary">
+    <x-slot name="title">{{ __('Identitas K2') }}</x-slot>
+    <a href="" class="btn btn-primary float-right mb-3">{{ __('Tambah Data') }}</a>
+    <div class="table-responsive">
+        <table class="table table-striped" id="tableK2">
+            <thead>
+                <tr align="center">
+                    <th width="5%" class="pt-2 text-center">{{ __('No') }}</th>
+                    <th width="30%">{{ __('Nama Ibu') }}</th>
+                    <th>{{ __('Bulan') }}</th>
+                    <th>{{ __('Nama Ibu') }}</th>
+                    <th>{{ __('Umur') }}</th>
+                    <th>{{ __('Alamat') }}</th>
+                    <th>{{ __('Nama Suami') }}</th>
+                    <th>{{ __('Tgl K4') }} </th>
+                    <th>{{ __('Masalah') }}</th>
+                </tr>
+            </thead>
+            <tbody align="center"></tbody>
+
+        </table>
+    </div>
+</x-card>
