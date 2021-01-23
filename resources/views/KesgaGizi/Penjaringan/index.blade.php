17fe010f4059e6cfaf50dc317082c0981a168087
zandri081@gmail.com.com <zandri081@gmail.com>
2021-01-23 22:26:45 +0700

    update penjaringan

--- resources/views/KesgaGizi/Penjaringan/index.blade.php
+++ resources/views/KesgaGizi/Penjaringan/index.blade.php
@@ -1,6 +1,6 @@
 <x-app-layout>
     <x-slot name="header">
-        {{ __('LAPORAN LB1 DAN P2P PTMA') }}
+        {{ __('Penjaringan') }}
     </x-slot>
     <x-slot name="button">
         <a href="{{ route('lap-bayi.create') }}" class="float-right btn btn-primary">Tambah Data</a>
@@ -9,7 +9,7 @@
 
         <div class="embed-responsive embed-responsive-16by9">
             <iframe class="embed-responsive-item"
-                src="https://docs.google.com/spreadsheets/d/13t4xjWHS7ULWUHO8eU4KmJwnijiGYuuvf_KTNHt_Kr4/edit?usp=sharing&amp;headers=false"></iframe>
+                src="https://docs.google.com/spreadsheets/d/1-TzGjBgiWTQaoNRyKsiHgjJk1sIleT-2cBwA8DLNIaI/edit?usp=sharing&amp;headers=false"></iframe>
         </div>
     </x-card>
     @push('js')
